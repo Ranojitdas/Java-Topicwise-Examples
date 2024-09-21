14eb12e4af0d354e0f77db887acd3340d6c4c80b
Ranojit <145350613+Ranojitdas@users.noreply.github.com>
2024-09-21 21:18:45 +0530

    Add files via upload

---
+++ Encapsulation/AccessModifierPractice/PrivateModifier.java
@@ -0,0 +1,9 @@
+package AccessModifierPractice;
+
+public class PrivateModifier {
+  static private int c = 30;
+
+    public static void main(String[] args) {
+        System.out.println(c);
+    }
+}
