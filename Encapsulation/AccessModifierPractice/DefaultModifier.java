43b6233408bdaf75be810dac97c624282afa651f
Ranojit <145350613+Ranojitdas@users.noreply.github.com>
2024-09-22 20:57:24 +0530

    Add files via upload

---
+++ Encapsulation/AccessModifierPractice/DefaultModifier.java
@@ -0,0 +1,9 @@
+package AccessModifierPractice;
+
+import java.util.Scanner;
+
+public class DefaultModifier {
+    Scanner input = new Scanner(System.in);
+    int a = input.nextInt();
+    int b = 20;
+}
